9710311f56e274a7febf7b16dc2a55cf962dcba2
Minh-Tam Tran <tran.minhtam.tenshi@gmail.com>
2020-08-03 02:01:17 +0200

    dotnet project templates

---
+++ templates/startup/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloWorld.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var serviceProvider = new ServiceCollection()
+				.BuildServiceProvider();
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
