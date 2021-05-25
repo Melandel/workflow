eac2a154035468836175b4b7201a76ed97beb989
Minh-Tam TRAN <c_mtran@capsuletech.com>
2021-05-25 23:12:59 +0200

    deploy      (setup.ps1): remove errors and make deployment script fully functional

--- config/setup.ps1
+++ config/setup.ps1
@@ -44,14 +44,15 @@ function Add-Path([string]$Uri) {
 	}
 }
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 if ($null -eq (Get-Command '7z.exe' -ErrorAction SilentlyContinue)) {
 	Write-Host '7z.exe not found.'
-    $7zipFolder = Join-Path $toolsFolder '/7-Zip'
-    $7zipInstaller = Join-Path $7zipFolder '/installer/7z.exe'
+	$7zipFolder = Join-Path $toolsFolder '/7-Zip'
+	$7zipInstaller = Join-Path $7zipFolder '/installer/7z.exe'
 	Download-File -Uri 'https://www.7-zip.org/a/7z1900-x64.exe' -Target $7zipInstaller
-    $7zipFolder = Join-Path $toolsFolder '/7-Zip/'
-    & $7zipInstaller /S "/D=$7zipFolder"
-    Set-Alias 7z (Join-Path $7zipFolder '7z.exe')
+	$7zipFolder = Join-Path $toolsFolder '/7-Zip/'
+	& $7zipInstaller /S "/D=$7zipFolder"
+	Set-Alias 7z (Join-Path $7zipFolder '7z.exe')
 	Add-Path('./7-zip')
 	Write-Host "`n"
 }
@@ -78,11 +79,23 @@ if ($null -eq (Get-Command "git.exe" -ErrorAction SilentlyContinue)) {
 	(Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/latest').Content > $tmp
 	$gitRelativeUri = (Select-String -Path $tmp -Pattern 'href="(.*download.*64.*\.exe?)"' -List).Matches.Groups[1].Value
 	Download-File -Uri "https://github.com$gitRelativeUri" -Target './git/installer.exe'
-	& './git/installer.exe' ('/Dir=' + (Resolve-Path './git').Path) /Group=git /VERYSILENT
-	# Add-Path('./git/bin'); We do this later on
+	Start-Process './git/installer.exe' -argumentlist "$('/Dir=' + (Resolve-Path './git').Path) /Group=git /VERYSILENT" -Wait
+	Add-Path('./git/bin')
 	Remove-Item $tmp
 	Write-Host "`n"
+	if (-not (Test-Path ../.git)) {
+		Write-Host -NoNewLine 'Cloning Desktop...'
+		Set-Location ..
+		& $toolsFolder/git/bin/git init | Out-Null
+		& $toolsFolder/git/bin/git remote add origin https://github.com/Melandel/workflow | Out-Null
+		& $toolsFolder/git/bin/git pull origin master 2>$null
+		Write-Host -NoNewLine 'OK.'
+		Set-Location $toolsFolder
+		Write-Host "`n"
+	}
 }
+
+
 
 if ($null -eq (Get-Command "vim.exe" -ErrorAction SilentlyContinue)) {
 	Write-Host 'Vim not found.'
@@ -96,12 +109,9 @@ if ($null -eq (Get-Command "vim.exe" -ErrorAction SilentlyContinue)) {
 	Remove-Item	-Recurse './vim/vim'
 	New-Item -ItemType SymbolicLink -Path (Join-Path './vim' '_vimrc') -Target (Join-Path $env:HOMEPATH 'Desktop/config/my_vimrc.vim') | Out-Null
 	Add-Path('./vim')
-    $vimrc = Join-Path $toolsFolder '_vimrc'
-    $my_vimrc = Join-Path $env:HOMEPATH 'Desktop/config/my_vimrc.vim'
-    New-Item -ItemType Junction -Path "$vimrc" -Target "$my_vimrc"
-    $ultisnips = Join-Path $env:HOMEPATH 'vimfiles/ultisnips'
-    $my_ultisnips= Join-Path $env:HOMEPATH 'Desktop/snippets'
-    New-Item -ItemType Junction -Path "$ultisnips" -Target "$my_ultisnips"
+	$ultisnips = Join-Path $env:HOMEPATH 'vimfiles/ultisnips'
+	$my_ultisnips= Join-Path $env:HOMEPATH 'Desktop/snippets'
+	New-Item -ItemType Junction -Path "$ultisnips" -Target "$my_ultisnips" -ErrorAction SilentlyContinue | Out-Null
 	Remove-Item $tmp
 	Write-Host "`n"
 }
@@ -133,24 +143,6 @@ if ($null -eq (Get-Command "fzf.exe" -ErrorAction SilentlyContinue)) {
 	Write-Host "`n"
 }
 
-if ($null -eq (Get-Command "less.exe" -ErrorAction SilentlyContinue)) {
-	Write-Host 'Less not found.'
-	Download-File -Uri 'https://downloads.sourceforge.net/project/gnuwin32/less/394/less-394-bin.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fgnuwin32%2Ffiles%2Fless%2F394%2Fless-394-bin.zip%2Fdownload%3Fuse_mirror%3Dkumisystems%26download%3D&ts=1598032639' -Target './less/less.zip'
-	7z x ('-o' +'less') './less/less.zip' | Out-Null
-	Download-File -Uri 'https://downloads.sourceforge.net/project/gnuwin32/less/394/less-394-dep.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fgnuwin32%2Ffiles%2Fless%2F394%2Fless-394-dep.zip%2Fdownload%3Fuse_mirror%3Dmaster%26download%3D&ts=1598033278' -Target './less/less.deps.zip'
-	7z x ('-o' +'less') './less/less.deps.zip' | Out-Null
-	Add-Path('./less/bin')
-	Write-Host "`n"
-}
-
-if ($null -eq (Get-Command "tree.exe" -ErrorAction SilentlyContinue)) {
-	Write-Host 'Tree not found.'
-	Download-File -Uri 'https://downloads.sourceforge.net/project/gnuwin32/tree/1.5.2.2/tree-1.5.2.2-bin.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fgnuwin32%2Ffiles%2Ftree%2F1.5.2.2%2Ftree-1.5.2.2-bin.zip%2Fdownload%3Fuse_mirror%3Dnetcologne&ts=1598033561' -Target './tree/tree.zip'
-	7z x ('-o' +'tree') './tree/tree.zip' | Out-Null
-	Add-Path('./tree/bin')
-	Write-Host "`n"
-}
-
 if ($null -eq (Get-Command "nuget.exe" -ErrorAction SilentlyContinue)) {
 	Write-Host 'Nuget not found.'
 	Download-File -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -Target './nuget/nuget.exe'
@@ -160,14 +152,16 @@ if ($null -eq (Get-Command "nuget.exe" -ErrorAction SilentlyContinue)) {
 
 if ($null -eq (Get-Command "python3.exe" -ErrorAction SilentlyContinue)) {
 	Write-Host 'Python3 not found.'
-	$vimPythonThreeVersionInfo = (vim --version | Select-String -Pattern '-DDYNAMIC_PYTHON3_DLL=\\"python(\d)(\d)\.dll\\"' -List).Matches
+	$vimPythonThreeVersionInfo = (.\vim\vim --version | Select-String -Pattern '-DDYNAMIC_PYTHON3_DLL=\\"python(\d)(\d)\.dll\\"' -List).Matches
 	$vimPythonMajorVersion = $vimPythonThreeVersionInfo.Groups[1].Value
 	$vimPythonMinorVersion = $vimPythonThreeVersionInfo.Groups[2].Value
 	$tmp = (Join-Path $toolsFolder 'python3.latestreleases.html')
 	(Invoke-WebRequest -Uri 'https://www.python.org/downloads/windows/').Content > $tmp
 	$python3Uri = (Select-String -Path $tmp -Pattern "href=`"(.*python-$vimPythonMajorVersion\.$vimPythonMinorVersion.*64.*\.exe?)`"" -List).Matches.Groups[1].Value
 	Download-File -Uri $python3Uri -Target './python3/installer.exe'
-	& './python3/installer.exe' /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 ('TargetDir=' + (Join-Path (Get-Location) 'python3'))
+	Start-Process './python3/installer.exe' -argumentlist "/quiet InstallAllUsers=0 PrependPath=1 Include_test=0 $('TargetDir=' + (Join-Path (Get-Location) 'python3'))" -Wait
+	Add-Path('./python3')
+	Remove-Item $tmp
 	Write-Host "`n"
 }
 
@@ -194,41 +188,26 @@ if ($null -eq (Get-Command "dot.exe" -ErrorAction SilentlyContinue)) {
 	Write-Host 'Graphviz not found.'
 	$tmp = (Join-Path $toolsFolder 'graphviz.releases.html')
 	(Invoke-WebRequest -Uri 'https://www2.graphviz.org/Packages/stable/windows/10/msbuild/Release/Win32').Content > $tmp
-    $fileName = (Select-String -Path $tmp -Pattern "href=`"(.*graphviz.*)`"" -List).Matches.Groups[1].Value
+	$fileName = (Select-String -Path $tmp -Pattern "href=`"(.*graphviz.*)`"" -List).Matches.Groups[1].Value
 	Download-File -Uri "https://www2.graphviz.org/Packages/stable/windows/10/msbuild/Release/Win32/$fileName" -Target './graphviz/graphviz.zip'
 	7z x ('-o' +'graphviz') './graphviz/graphviz.zip' | Out-Null
 	$installFolder = (Split-Path (Get-ChildItem -Recurse -Path './graphviz' dot.exe).FullName -Parent) | Split-Path -Parent
 	Move-Item "$installFolder/*" './graphviz'
 	Remove-Item	-Recurse './graphviz/Graphviz'
 	Add-Path('./graphviz/bin')
+	Remove-Item $tmp
 	Write-Host "`n"
 }
 
-if ($null -eq (Get-Command "yes.exe" -ErrorAction SilentlyContinue)) {
-	Write-Host 'Gnu CoreUtils not found.'
-    $installer = Join-Path $toolsFolder '/coreutils/installer.exe'
-	Download-File -Uri 'https://downloads.sourceforge.net/project/gnuwin32/sed/4.2.1/sed-4.2.1-setup.exe?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fgnuwin32%2Ffiles%2Flatest%2Fdownload&ts=1605993279' -Target "$installer"
-	$folder = [IO.Path]::Combine($env:HOMEDRIVE, $env:HOMEPATH, 'Desktop/tools/coreutils')
-    & $installer /VERYSILENT "/D=`"$folder`""
-    New-Item -ItemType Junction -Path "$(Join-Path $toolsFolder 'coreutils/bin/expandtabs.exe')" -Target "$(Join-Path $toolsFolder 'coreutils/bin/expand.exe')"
-	Write-Host "`n"
-}
-
-Add-Path('./coreutils/bin')
-Add-Path('./git/bin')
 Remove-Item './autohotkey/ahk.zip' -ErrorAction SilentlyContinue
 Remove-Item './firefox/installer.exe' -ErrorAction SilentlyContinue
 Remove-Item './git/installer.exe' -ErrorAction SilentlyContinue
 Remove-Item './vim/vim.zip' -ErrorAction SilentlyContinue
 Remove-Item './ripgrep/ripgrep.zip' -ErrorAction SilentlyContinue
 Remove-Item './fzf/fzf.zip' -ErrorAction SilentlyContinue
-Remove-Item './less/less.zip' -ErrorAction SilentlyContinue
-Remove-Item './less/less.deps.zip' -ErrorAction SilentlyContinue
-Remove-Item './tree/tree.zip' -ErrorAction SilentlyContinue
 Remove-Item './python3/installer.exe' -ErrorAction SilentlyContinue
 Remove-Item './omnisharp/omnisharp.zip' -ErrorAction SilentlyContinue
 Remove-Item './graphviz/graphviz.zip' -ErrorAction SilentlyContinue
-Remove-Item	'./coreutils/installer.exe' -ErrorAction SilentlyContinue
 
 Set-Location $cd
 Write-Host 'Over.'
