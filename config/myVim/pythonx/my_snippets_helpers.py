2a96ae7394a3a83167931351a814a17297bf8cce
Minh-Tam TRAN <minh-tam.tran@younited-credit.fr>
2022-12-23 13:17:57 +0100

    🐛 fix             (plantuml-snippets): fix visual selection indentation
    💌 Motivation: readability

--- config/myVim/pythonx/my_snippets_helpers.py
+++ config/myVim/pythonx/my_snippets_helpers.py
@@ -1,9 +1,32 @@
 import os.path
 import uuid
+import textwrap
 
 def rnd():
  return str(uuid.uuid4())
 
 def path_to_namespace(path):
  return os.path.dirname(path).replace('src\\', '').replace('\\', '.')
+
+def pascal_case(value):
+ return "".join(value.title().split())
+
+def newline_if_content(visual_text, placeholder):
+ if visual_text != "":
+  return "\n"
+ elif placeholder == "":
+  return ""
+ else:
+  return "\n"
+
+def space_or_newline_depending_on_content(visual_text, placeholder):
+ if visual_text != "":
+  return "" if visual_text.endswith("\n") else "\n"
+ elif placeholder == "":
+  return " "
+ else:
+  return "\n"
+
+def add_indent(text, indent="\t", nb=1):
+ return textwrap.indent(text, nb*indent)
 
